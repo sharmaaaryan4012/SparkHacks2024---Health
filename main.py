4aa80f703320802a081e9af0bc95f0451d3c8fd8
sharmaaaryan4012 <sharmaaaryan4012@gmail.com>
2024-02-10 00:38:17 -0600

    headers
    Added - Headers to files

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Project: Super Health App
+Creators: Aaryan Sharma, Ayush Bhardwaj
+SparkHacks-2024 (University of Illinois at Chicago)
+"""
+
+
---
+++ mindful.py
@@ -0,0 +1,5 @@
+"""
+Project: Super Health App
+Creators: Aaryan Sharma, Ayush Bhardwaj
+SparkHacks-2024 (University of Illinois at Chicago)
+"""
