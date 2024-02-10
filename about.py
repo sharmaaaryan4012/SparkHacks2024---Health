2ecb6cbb6d2e5bfcb0bd306b4ed2a39d67915454
sharmaaaryan4012 <sharmaaaryan4012@gmail.com>
2024-02-10 03:37:44 -0600

    Updated - variable names of main and about

--- about.py
+++ about.py
@@ -7,16 +7,15 @@ SparkHacks-2024 (University of Illinois at Chicago)
 
 import tkinter as tk
 
-
 class AboutPage(tk.Frame):
     def __init__(self, master, backCallback):
-        super().__init__(master)
+        super().__init__(master, bg='white')
         self.master = master
         self.backCallback = backCallback
         self.createWidgets()
 
     def createWidgets(self):
-        description = (
+        descriptionText = (
             "Project: Super Health App\n"
             "Creators: Aaryan Sharma, Ayush Bhardwaj\n"
             "Event: SparkHacks-2024\n"
@@ -27,14 +26,14 @@ class AboutPage(tk.Frame):
             "mindfulness exercises."
         )
 
-        title_label = tk.Label(self, text="About Super Health App", font=('Helvetica', 18, 'bold'))
-        title_label.pack(pady=20)
+        titleLabel = tk.Label(self, text="About Super Health App", bg='white', fg='black', font=('Helvetica', 18, 'bold'))
+        titleLabel.pack(pady=20)
 
-        description_label = tk.Label(self, text=description, font=('Helvetica', 12), justify=tk.LEFT)
-        description_label.pack(pady=20, padx=20)
+        descriptionLabel = tk.Label(self, text=descriptionText, bg='white', fg='black', font=('Helvetica', 12), justify=tk.LEFT)
+        descriptionLabel.pack(pady=20, padx=20)
 
-        back_button = tk.Button(self, text="Back", command=self.goBack)
-        back_button.pack(pady=20)
+        backButton = tk.Button(self, text="Back", command=self.goBack)
+        backButton.pack(pady=20)
 
     def goBack(self):
         self.destroy()
--- main.py
+++ main.py
@@ -18,83 +18,69 @@ class MainPage(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Super Health App')
+        self.geometry('4000x2250')
         self.state('zoomed')
-        self.configure(bg = "white")
+        self.configure(bg='white')
         self.createWidgets()
 
     def createWidgets(self):
-        welcome_label = tk.Label(self, text="Welcome to the Onboard with the All-in-one Health Companion!", fg='black',
-                                 bg='white', font=('Helvetica', 25, 'bold'))
-        welcome_label.pack(pady=20)
+        welcomeLabel = tk.Label(self, text="Welcome to Onboard with the All-in-one Health Companion!", fg='black', bg='white', font=('Helvetica', 25, 'bold'))
+        welcomeLabel.pack(pady=20)
 
-        # Create a container frame for the buttons to center them
-        buttons_frame = tk.Frame(self)
-        buttons_frame.pack(expand=True)
+        buttonsFrame = tk.Frame(self, bg='white')
+        buttonsFrame.pack(expand=True)
 
-        doctors_button = tk.Button(buttons_frame, text="Doctors", font=('Helvetica', 12), command=self.gotoDoctors)
-        doctors_button.pack(pady=10, ipadx=20, ipady=10, fill='x')
+        doctorsButton = tk.Button(buttonsFrame, text="Doctors", font=('Helvetica', 12), command=self.gotoDoctors, bg='white')
+        doctorsButton.pack(pady=10, ipadx=20, ipady=10, fill='x')
 
-        prescription_button = tk.Button(buttons_frame, text="Prescription Manager", font=('Helvetica', 12),
-                                        command=self.gotoPrescription)
-        prescription_button.pack(pady=10, ipadx=20, ipady=10, fill='x')
+        prescriptionButton = tk.Button(buttonsFrame, text="Prescription Manager", font=('Helvetica', 12), command=self.gotoPrescription, bg='white')
+        prescriptionButton.pack(pady=10, ipadx=20, ipady=10, fill='x')
 
-        reminders_button = tk.Button(buttons_frame, text="Reminders", font=('Helvetica', 12),
-                                     command=self.gotoReminders)
-        reminders_button.pack(pady=10, ipadx=20, ipady=10, fill='x')
+        remindersButton = tk.Button(buttonsFrame, text="Reminders", font=('Helvetica', 12), command=self.gotoReminders, bg='white')
+        remindersButton.pack(pady=10, ipadx=20, ipady=10, fill='x')
 
-        mindful_button = tk.Button(buttons_frame, text="Mindful Space", font=('Helvetica', 12),
-                                   command=self.gotoMindful)
-        mindful_button.pack(pady=10, ipadx=20, ipady=10, fill='x')
+        mindfulButton = tk.Button(buttonsFrame, text="Mindful Space", font=('Helvetica', 12), command=self.gotoMindful, bg='white')
+        mindfulButton.pack(pady=10, ipadx=20, ipady=10, fill='x')
 
-        # For the bottom buttons, we'll create another frame
-        bottom_frame = tk.Frame(self)
-        bottom_frame.pack(side='bottom', fill='x', expand=False)
+        bottomFrame = tk.Frame(self, bg='white')
+        bottomFrame.pack(side='bottom', fill='x', expand=False)
 
-        about_button = tk.Button(bottom_frame, text="About", font=('Helvetica', 12), command=self.gotoAbout)
-        about_button.pack(side='left', padx=10, pady=10)
+        aboutButton = tk.Button(bottomFrame, text="About", font=('Helvetica', 12), command=self.gotoAbout, bg='white')
+        aboutButton.pack(side='left', padx=10, pady=10)
 
-        space_game_button = tk.Button(bottom_frame, text="Space Game", font=('Helvetica', 12),
-                                      command=self.gotoSpaceGame)
-        space_game_button.pack(side='right', padx=10, pady=10)
+        spaceGameButton = tk.Button(bottomFrame, text="Space Game", font=('Helvetica', 12), command=self.launchSpaceGame, bg='white')
+        spaceGameButton.pack(side='right', padx=10, pady=10)
 
     def gotoDoctors(self):
         messagebox.showinfo("Go to Doctors", "This will go to the Doctors section.")
 
-
     def gotoPrescription(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-
-        pres_page = PrescriptionPage(self, self.CallBack)
-        pres_page.pack(fill=tk.BOTH, expand=True)
-
+        self.clearWidgets()
+        PrescriptionPage(self, self.callback).pack(fill=tk.BOTH, expand=True)
 
     def gotoReminders(self):
         messagebox.showinfo("Go to Reminders", "This will go to the Reminders section.")
 
-
     def gotoMindful(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-        MindfulPage(self, self.CallBack)
-
+        self.clearWidgets()
+        MindfulPage(self, self.callback).pack(fill=tk.BOTH, expand=True)
 
     def gotoAbout(self):
-        for widget in self.winfo_children():
-            widget.destroy()
-        AboutPage(self, self.CallBack).pack(fill=tk.BOTH, expand=True)
+        self.clearWidgets()
+        AboutPage(self, self.callback).pack(fill=tk.BOTH, expand=True)
 
+    def launchSpaceGame(self):
+        currentDir = os.path.dirname(os.path.abspath(__file__))
+        spaceGamePath = os.path.join(currentDir, 'SpaceGame', 'space.py')
+        subprocess.Popen([sys.executable, spaceGamePath])
 
-    def gotoSpaceGame(self):
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        space_game_path = os.path.join(current_dir, 'SpaceGame', 'space.py')
-        subprocess.Popen([sys.executable, space_game_path])
-
-
-    def CallBack(self):
-        for widget in self.winfo_children():
-            widget.destroy()
+    def callback(self):
+        self.clearWidgets()
         self.createWidgets()
+
+    def clearWidgets(self):
+        for widget in self.winfo_children():
+            widget.destroy()
 
 if __name__ == '__main__':
     app = MainPage()
